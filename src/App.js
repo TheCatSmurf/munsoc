9f04eb1b080a3190eccd74bd1efcb0c8f5d35aa6
Aravind B Krishnan <aravindkrishnan953@gmail.com>
2024-08-26 17:54:06 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+// Import images
+import thumbnail1 from './images/thumbnail1.jpg';
+import thumbnail2 from './images/thumbnail2.jpeg';
+import thumbnail3 from './images/thumbnail3.jpeg';
+import thumbnail4 from './images/thumbnail4.jpg';
+import thumbnail5 from './images/thumbnail5.jpeg';
+import thumbnail6 from './images/thumbnail6.jpg';
+import caricature1 from './images/caricature1.jpg';
+import caricature2 from './images/caricature2.jpg';
+import caricature3 from './images/caricature3.jpg';
+// Import Modal
+import Modal from 'react-modal';
+import QuizGame from './components/QuizGame.js';
+
+Modal.setAppElement('#root'); // This is for accessibility purposes
+
+const handleLinkClick = (e) => {
+  e.target.classList.add('gavel-click');
+
+  // Remove the class after the animation completes to reset
+  setTimeout(() => {
+      e.target.classList.remove('gavel-click');
+  }, 200); // Match the duration of the animation in milliseconds
+};
+
+function App() {
+  // State to manage modal visibility and selected image
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <h1>MUNSoc</h1>
+        </div>
+        <nav className="App-nav">
+          <a href="#home" onClick={handleLinkClick}>Home</a>
+          <a href="#about" onClick={handleLinkClick}>About Us</a>
+          <a href="#contact" onClick={handleLinkClick}>Contact Us</a>
+          <a href="#join" onClick={handleLinkClick}>Join Us</a>
+        </nav>
+      </header>
+
+      <main className="App-body">
+        {/* Article Boxes */}
+        <div className="article-box">
+          <img className="article-thumbnail" src={thumbnail1} alt="Article 1" />
+          <div className="article-headline">Take a look at the most iconic moments throughout the FIFA World Cups</div>
+        </div>
+        
+        <div className="article-box">
+          <img className="article-thumbnail" src={thumbnail2} alt="Article 2" />
+          <div className="article-headline">Israel and Hezbollah trade their most intense fire in months and then pull back</div>
+        </div>
+        <br />
+        <div className="article-box">
+          <img className="article-thumbnail" src={thumbnail4} alt="Article 4" />
+          <div className="article-headline">Reviving Diplomacy: MEC's First Post-COVID MUN Sparks Unforgettable Moments and Connections </div>
+        </div>
+          
+
+
+
+        {/* Caricature Section */}
+        <section className="caricature-section">
+          <h2 style={{ color: 'white' }}>Caricatures</h2>
+          <div className="caricature-box" onClick={() => openModal(caricature1)}>
+            <img className="caricature-image" src={caricature1} alt="Caricature 1" />
+            <div className="caricature-title">Pee-suasive Politics</div>
+          </div>
+          <div className="caricature-box" onClick={() => openModal(caricature2)}>
+            <img className="caricature-image" src={caricature2} alt="Caricature 2" />
+            <div className="caricature-title">Out on a Limb</div>
+          </div>
+          <div className="caricature-box" onClick={() => openModal(caricature3)}>
+            <img className="caricature-image" src={caricature3} alt="Caricature 3" />
+            <div className="caricature-title">Uncle's Sketchy Business: Drawing the Line at Arrest!</div>
+          </div>
+        </section>
+        <QuizGame />  
+      </main>
+
+      <footer className="App-footer">
+        <div className="social-media-links">
+          <h3>Follow Us:</h3>
+          <a href="https://www.instagram.com/mec_munsoc/" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
+            Instagram
+          </a>
+          <a href="https://x.com/reachmecmun?lang=en" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
+            X
+          </a>
+          <a href="https://www.linkedin.com/company/mecmunsoc/?originalSubdomain=in" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>
+            LinkedIn
+          </a>
+        </div>
+        <p>© 2024 MUNSoc. All Rights Reserved </p>
+      </footer>
+
+      {/* Modal for Enlarged Image */}
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="Modal" overlayClassName="Overlay">
+        <button onClick={closeModal} style={{ float: 'right', background: 'none', border: 'none', color: 'white', fontSize: '24px' }}>×</button>
+        <img src={selectedImage} alt="Enlarged" style={{ width: '100%', height: 'auto' }} />
+      </Modal>
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ src/components/QuizGame.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+import './QuizGame.css';
+const quizQuestions = [
+    {
+        question: "What does MUN stand for?",
+        options: ["Model United Nations", "Modern United Nations", "Model Universal Network", "Modern Universal Network"],
+        answer: "Model United Nations",
+    },
+    {
+        question: "Which country is not a permanent member of the UN Security Council?",
+        options: ["USA", "Russia", "Germany", "China"],
+        answer: "Germany",
+    },
+    {
+        question: "Which country was the first to sign the United Nations Charter in 1945?",
+        options: [
+          "United States",
+          "United Kingdom",
+          "France",
+          "China"
+        ],
+        answer: "United States",
+      },
+      {
+        question: "What is the main purpose of the United Nations?",
+        options: [
+          "To promote world peace",
+          "To establish military alliances",
+          "To regulate trade",
+          "To manage national borders"
+        ],
+        answer: "To promote world peace",
+      },
+      {
+        question: "How many permanent members are there in the UN Security Council?",
+        options: [
+          "5",
+          "10",
+          "15",
+          "20"
+        ],
+        answer: "5",
+      },
+      {
+        question: "Which of the following is NOT a primary organ of the United Nations?",
+        options: [
+          "General Assembly",
+          "Security Council",
+          "World Health Organization",
+          "International Court of Justice"
+        ],
+        answer: "World Health Organization",
+      },
+      {
+        question: "What document outlines the fundamental rights of individuals recognized by the UN?",
+        options: [
+          "Universal Declaration of Human Rights",
+          "International Covenant on Civil and Political Rights",
+          "Charter of the United Nations",
+          "Declaration of the Rights of the Child"
+        ],
+        answer: "Universal Declaration of Human Rights",
+      },
+      {
+        question: "Which country hosted the first Model United Nations conference?",
+        options: [
+          "United States",
+          "Canada",
+          "United Kingdom",
+          "France"
+        ],
+        answer: "United States",
+      },
+      {
+        question: "Which event is often considered the starting point of the United Nations?",
+        options: [
+          "The Treaty of Versailles",
+          "The League of Nations",
+          "The San Francisco Conference",
+          "The signing of the Kyoto Protocol"
+        ],
+        answer: "The San Francisco Conference",
+      },
+      {
+        question: "What is the name of the UN agency responsible for coordinating international public health?",
+        options: [
+          "UNICEF",
+          "UNESCO",
+          "WHO",
+          "FAO"
+        ],
+        answer: "WHO",
+      },
+      {
+        question: "Which of the following is a function of the UN General Assembly?",
+        options: [
+          "To make binding decisions",
+          "To elect non-permanent members of the Security Council",
+          "To oversee the UN budget",
+          "To elect non-permanent members of the Security Council and to oversee the UN budget"
+        ],
+        answer: "Both B and C",
+      },
+      {
+        question: "Which treaty established the International Criminal Court (ICC)?",
+        options: [
+          "The Rome Statute",
+          "The Geneva Conventions",
+          "The Hague Convention",
+          "The Kyoto Protocol"
+        ],
+        answer: "The Rome Statute",
+      },
+      {
+        question: "What is the maximum term length for a UN Secretary-General?",
+        options: [
+          "5 years",
+          "10 years",
+          "15 years",
+          "There is no limit"
+        ],
+        answer: "5 years",
+      },
+      {
+        question: "Which international organization focuses on economic cooperation and development?",
+        options: [
+          "NATO",
+          "WTO",
+          "OPEC",
+          "UNDP"
+        ],
+        answer: "WTO",
+      },
+];
+
+function QuizGame() {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
+
+    const handleAnswerOptionClick = (selectedOption) => {
+        if (selectedOption === quizQuestions[currentQuestion].answer) {
+            setScore(score + 1);
+        }
+
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < quizQuestions.length) {
+            setCurrentQuestion(nextQuestion);
+        } else {
+            setShowScore(true);
+        }
+    };
+
+    return (
+        <div className="quiz-container">
+            {showScore ? (
+                <div className="score-section">
+                    You scored {score} out of {quizQuestions.length}
+                </div>
+            ) : (
+                <>
+                    <div className="question-section">
+                        <div className="question-count">
+                            <span>Question {currentQuestion + 1}</span>/{quizQuestions.length}
+                        </div>
+                        <div className="question-text">{quizQuestions[currentQuestion].question}</div>
+                    </div>
+                    <div className="answer-section">
+                        {quizQuestions[currentQuestion].options.map((option, index) => (
+                            <button key={index} onClick={() => handleAnswerOptionClick(option)}>
+                                {option}
+                            </button>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
+    );
+}
+
+export default QuizGame;
